a342de9867abf091446f3a43192499b87b362777
Evellyn Küster <evellynkusterlimadeoliveira@gmail.com>
2025-04-01 21:38:26 -0300

    feat: adiciona script de banco de dados para as tabelas de categorias e produtos.

---
+++ MultApps/MODEL/MultApps.Models/Scripts/banco_de_dados.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS multapps_dev;
+USE multapps_dev;
+
+CREATE TABLE IF NOT EXISTS  categoria(
+	id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(250) NOT NULL,
+    data_criacao TIMESTAMP NOT NULL,
+    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    status ENUM ('inativo', 'ativo', 'excluido') NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS  produto(
+	id INT NOT NULL AUTO_INCREMENT,
+	categoria_id INT NOT NULL,
+    nome VARCHAR(250) NOT NULL,
+    preco DECIMAL NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    data_criacao TIMESTAMP NOT NULL,
+    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    status ENUM ('inativo', 'ativo', 'excluido') NOT NULL,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
